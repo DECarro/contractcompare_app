b0a735785aacba0844fd93d14b4a28f03a44c43e
Danielle Sekslsky <carro.danielle@gmail.com>
2014-09-21 16:45:21 -0400

    adjust header again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def landing_page
   	@featured_product = Product.find(1)
+  	whyus = Product.find(1)
   end
 
   def contact
@@ -12,7 +13,7 @@ class PagesController < ApplicationController
   def faq
   end
 
-  
+
 
  
 end
