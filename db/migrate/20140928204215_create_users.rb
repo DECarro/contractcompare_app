bcc37f05252fab4ec5c537b35fc0dca392288381
Danielle Sekslsky <carro.danielle@gmail.com>
2014-10-10 13:54:41 -0400

    devise

--- db/migrate/20140928204215_create_users.rb
+++ db/migrate/20140928204215_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
     	t.string :fname
     	t.string :lname
-    	t.string :email
+    	t.string :email,         null: false, default: ""     
     	
       t.timestamps
     end
